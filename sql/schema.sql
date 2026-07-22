1c47b80445fd9894486adcce0cb65abf69856b2d
Marcella Moyano <140667755+Marcemoyano@users.noreply.github.com>
2026-07-22 10:08:00 -0300

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,38 +3,35 @@
 -- SGBD: PostgreSQL
 -- ============================================================
 
--- 1. Tabela: maquinas
+-- 1. Tabela de Máquinas
 CREATE TABLE maquinas (
-    maquina_id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    maquina_id INT PRIMARY KEY,
+    nome VARCHAR(100),
     modelo VARCHAR(50),
-    status VARCHAR(20) NOT NULL
+    status VARCHAR(20)
 );
 
--- 2. Tabela: equipe_tecnica
+-- 2. Tabela da Equipe Técnica
 CREATE TABLE equipe_tecnica (
-    tecnico_id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    tecnico_id INT PRIMARY KEY,
+    nome VARCHAR(100),
     especialidade VARCHAR(50)
 );
 
--- 3. Tabela Fato: chamados_manutencao
+-- 3. Tabela de Chamados de Manutenção
 CREATE TABLE chamados_manutencao (
-    chamado_id SERIAL PRIMARY KEY,
-    maquina_id INT NOT NULL,
-    tecnico_id INT NOT NULL,
-    tipo_falha VARCHAR(50) NOT NULL,
-    status_chamado VARCHAR(20) NOT NULL,
-    horas_reparo INT DEFAULT 0,
-    CONSTRAINT fk_chamados_maquinas FOREIGN KEY (maquina_id) REFERENCES maquinas (maquina_id) ON DELETE CASCADE,
-    CONSTRAINT fk_chamados_equipe FOREIGN KEY (tecnico_id) REFERENCES equipe_tecnica (tecnico_id) ON DELETE RESTRICT
+    chamado_id INT PRIMARY KEY,
+    maquina_id INT,
+    tecnico_id INT,
+    tipo_falha VARCHAR(50),
+    status_chamado VARCHAR(20),
+    horas_reparo INT
 );
 
--- 4. Tabela: custos_manutencao
+-- 4. Tabela de Custos
 CREATE TABLE custos_manutencao (
-    custo_id SERIAL PRIMARY KEY,
-    chamado_id INT NOT NULL,
-    custo_pecas NUMERIC(10, 2) DEFAULT 0.00,
-    custo_obra NUMERIC(10, 2) DEFAULT 0.00,
-    CONSTRAINT fk_custos_chamados FOREIGN KEY (chamado_id) REFERENCES chamados_manutencao (chamado_id) ON DELETE CASCADE
+    custo_id INT PRIMARY KEY,
+    chamado_id INT,
+    custo_pecas DECIMAL(10, 2),
+    custo_obra DECIMAL(10, 2)
 );
